3c4e6feaf0614e1371242fb2ffbbe09ad965ecbc
Federico Rossi <f.rossi57@studenti.unipi.it>
2019-05-22 17:32:21 +0200

    Putting also human tags inside extracted

--- darkpolonet/extract.sh
+++ darkpolonet/extract.sh
@@ -1,10 +1,11 @@
-if [ "$#" -ne 2 ]; then
-    echo "Usage: extract.sh path_to_images path_to_txt_output"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: extract.sh path_to_images path_to_tags path_to_txt_output"
     exit
 fi
 
 path=$1
-outdir=$2
+tagsp=$2
+outdir=$3
 imgs="$path*.jpg"
 
 
@@ -13,8 +14,13 @@ echo "Going to extract bbox and tags for $size images"
 for f in $imgs; do
 	filename=$(basename $f)
 	filename="${filename%.*}"
-	dest="$outdir$filename.txt"
+	tagsf="tags$(echo $filename | grep -o -E '[0-9]+')"
+	dest="$outdir/$filename.txt"
 	echo Extracting from $filename
-	./darknet detect cfg/yolov3.cfg ./yolov3.weights $f 2> /dev/null  > $dest
+#	./darknet detect cfg/yolov3.cfg ./yolov3.weights $f 2> /dev/null  > $dest
+	echo Fetching human tags from $filename
+	source_tag="$tagsp/$tagsf.txt"
+	tags_content=$(tr -s '\r\n' ',' < $source_tag | sed -e 's/,$/\n/')
+	echo $tags_content >> $dest
 	echo Extracted to $dest
 done
