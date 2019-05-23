cba9034283fdfb1351b7334d7a4c7c97b1287134
Federico Rossi <f.rossi57@studenti.unipi.it>
2019-05-23 16:23:43 +0200

    Added methods to progressively build the detailed image

--- src/it/unipi/ing/mim/deep/DetailedImage.java
+++ src/it/unipi/ing/mim/deep/DetailedImage.java
@@ -1,13 +1,12 @@
 package it.unipi.ing.mim.deep;
 
-import java.awt.Image;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.bytedeco.opencv.opencv_core.Rect;
@@ -38,6 +37,7 @@ public class DetailedImage {
 			if(splittedYoloData.length < 2) break;
 			//Otherwise, first element is class-name, second is a bounding box.
 			classNames.add(splittedYoloData[0]);
+			System.out.println("Found class: "+splittedYoloData[0]+" in: "+splittedYoloData[1]);
 			//Second element is a list of int-coordinates
 			String[] coords = splittedYoloData[1].split(",");
 			int[] intCoords = Arrays.stream(coords).mapToInt(Integer::parseInt).toArray();
@@ -52,6 +52,35 @@ public class DetailedImage {
 		
 		metaBuferedReader.close();
 		metaReader.close();
+	}
+	
+	/*
+	 * Build detailed image from a single SearchHit
+	 */
+	public DetailedImage(String imageID,ArrayList<String> bboxes,String[] classes,String[] tags) {
+		for(String b:bboxes) {
+			String[] coords = b.split(",");
+			int[] intCoords = Arrays.stream(coords).mapToInt(Integer::parseInt).toArray();		
+			boundingBoxes.add(intCoords);
+		}
+		classNames = (ArrayList<String>)Arrays.asList(classes);
+		String imagePath = Parameters.SRC_FOLDER+File.pathSeparator+imageID;
+		content = imread(imagePath);
+		humanTags = tags;
+	}
+	
+	public DetailedImage(String imageID,String[] tags) {
+		String imagePath = Parameters.SRC_FOLDER+File.pathSeparator+imageID;
+		content = imread(imagePath);
+		humanTags = tags;
+		
+	}
+	
+	public void addBoundingBox(String bbox,String className) {
+		classNames.add(className);
+		String[] coords = bbox.split(",");
+		int[] intCoords = Arrays.stream(coords).mapToInt(Integer::parseInt).toArray();		
+		boundingBoxes.add(intCoords);		
 	}
 	
 	/*
@@ -84,16 +113,32 @@ public class DetailedImage {
 	public String serializeHumanTags() {
 		return String.join(",", humanTags);
 	}
+	
+	public ArrayList<String> serializeBoundingBoxes() {
+		ArrayList<String> bboxes = new ArrayList<String>();
+		for(int[] b: boundingBoxes) {
+			String[] arr = ((IntStream)Arrays.stream(b)).mapToObj(String::valueOf).toArray(String[]::new);
+			bboxes.add(String.join(",",arr));
+		}
+		return bboxes;
+	}
+	
+	public Mat getContent() {
+		return content;
+	}
 
-	/*public static void main(String[] args) throws IOException {
-		String yoloData = "data/img/extracted/im10001.txt";
-		String imageData = "data/img/mirflickr/im10001.jpg";
+	public static void main(String[] args) throws IOException {
+		String yoloData = "data/img/extracted/im10055.txt";
+		String imageData = "data/img/mirflickr/im10055.jpg";
 		DetailedImage di = new DetailedImage(new File(imageData), new File(yoloData));
 		ArrayList<Mat> rois = di.getRegionsOfInterest();
+		ArrayList<String> bboxes = di.serializeBoundingBoxes();
+		imshow("Original",di.getContent());
+		waitKey();
 		for(Mat r:rois) {
 			imshow("Region",r);
 			waitKey();
 		}
-	}*/
+	}
 	
 }
