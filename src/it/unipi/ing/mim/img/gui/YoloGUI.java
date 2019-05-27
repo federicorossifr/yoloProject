1a92e0f1c4be7b97061611f0c26e6ba1eb44ee4d
vincentdc95 <slv.vincent95@gmail.com>
2019-05-27 22:41:33 +0200

    added gridview. Now search by query and search by tag returns a collection of images onto the gui

--- src/it/unipi/ing/mim/img/gui/YoloGUI.java
+++ src/it/unipi/ing/mim/img/gui/YoloGUI.java
@@ -3,17 +3,29 @@ package it.unipi.ing.mim.img.gui;
 import javafx.scene.control.Button;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.lucene.queryparser.classic.ParseException;
 
 import it.unipi.ing.mim.deep.DNNExtractor;
+import it.unipi.ing.mim.deep.ImageUtils;
 import it.unipi.ing.mim.deep.ImgDescriptor;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
@@ -21,57 +33,90 @@ import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 import it.unipi.ing.mim.deep.Parameters;
+import it.unipi.ing.mim.img.elasticsearch.ElasticImgSearching;
 import it.unipi.ing.mim.img.elasticsearch.Pivots;
 
 public class YoloGUI extends Application {
 	
-	private TextField humanTagsUrl = new TextField();
-	private Button openButton = new Button("Search By Image");
+	private TextField humanTags = new TextField();
+	private Button openButton = new Button("Open an image");
 	private ImageView img = new ImageView();
 	private Button startSearch = new Button("Start Search");
-
+	private GridPane imageResults = new GridPane();
+	private ScrollPane scroll = new ScrollPane();
+		
 	private float[] imgFeatures;
-	private Pivots pivots;
-	private String textFeatures = "";
+	private File openedImage;
+	
+	private ElasticImgSearching eSearch; 
 	
 	@Override
 	public void start(Stage stage) {
 	
 		Label pathLabel = new Label("Insert Human Tag:");
-		HBox hbox = new HBox(20,new Label(""),pathLabel, humanTagsUrl );
+		HBox hbox = new HBox(20,new Label(""),pathLabel, humanTags );
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Open Resource File");
 		fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Image Files", "*.jpg"));
 		
+		try {
+			
+			eSearch = new ElasticImgSearching(it.unipi.ing.mim.deep.Parameters.PIVOTS_FILE, it.unipi.ing.mim.deep.Parameters.TOP_K_QUERY);
+		
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
 		openButton.setOnAction((ActionEvent ev) ->{
 			
-			File file = fileChooser.showOpenDialog(stage);
-	        if (file != null) {
-	        	Image i = new Image(file.toURI().toString());
+			openedImage = fileChooser.showOpenDialog(stage);
+	        if (openedImage != null) {
+	
+	        	Image i = new Image(openedImage.toURI().toString());
 	        	img.setImage(i);
 	        	
-	        	DNNExtractor extractor = new DNNExtractor();
-				
-				imgFeatures = extractor.extract(file, it.unipi.ing.mim.deep.Parameters.DEEP_LAYER);
-				
-				try {
-					
-					pivots = new Pivots(file);
-					ImgDescriptor imDes = new ImgDescriptor(imgFeatures, file.getName());
-					textFeatures = pivots.features2Text(imDes, it.unipi.ing.mim.deep.Parameters.TOP_K_QUERY);
-					
-				} catch (ClassNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
 	        }
 	    });
 		
 		startSearch.setOnAction((ActionEvent ev)-> {
 			
+			List<ImgDescriptor> searched = null;
+			ArrayList<Image> imageTemp = new ArrayList<Image>();
+			
+			if(!humanTags.getText().equals("")) {
+				searched = tagSearch(humanTags.getText());
+				
+			}else if(img.getImage() != null) {
+				searched = imageSearch(openedImage);
+			}
+			
+			for(ImgDescriptor i : searched) {
+				try {
+					imageTemp.add(ImageUtils.getDrawable(i));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			 int cols=7, colCnt = 0, rowCnt = 0;
+			 for (int i=0; i<imageTemp.size(); i++) {
+			    	ImageView imgIn = new ImageView(imageTemp.get(i));
+			    	imgIn.setFitHeight(200);
+			    	imgIn.setFitWidth(200);
+			    	imgIn.setPreserveRatio(true);
+			        imageResults.add(imgIn, colCnt, rowCnt);
+			        colCnt++;
+			        if (colCnt>cols) {
+			            rowCnt++;
+			            colCnt=0;
+			        }
+			}
+
 			
 		});
 		
@@ -80,17 +125,74 @@ public class YoloGUI extends Application {
 		img.setPreserveRatio(true);
 		HBox imgBox = new HBox(img);
 		imgBox.setStyle("-fx-border-color: red; -fx-border-width: 1; -fx-border-style: dotted;");
-		
+
 		HBox hbox1 = new HBox(20,openButton, imgBox);
-		VBox vbox = new VBox(40, hbox, hbox1, startSearch);
+		imageResults.setPadding(new Insets(10,10,10,10));
+		imageResults.setHgap(10);
+		imageResults.setVgap(10);
+		scroll.setContent(imageResults);
+		scroll.setPrefWidth(1500);
+		scroll.setPrefHeight(400);
+		VBox vbox = new VBox(40, hbox, hbox1, startSearch, scroll);
 		Group root = new Group(vbox);
-		Scene scene = new Scene(root, 800,800);
+		Scene scene = new Scene(root, 1500,800);
 		
 		stage.setScene(scene);
 		stage.setTitle("Yolo GUI");
 		stage.show();
 		
 	}
+	
+	private List<ImgDescriptor> tagSearch(String tag) {
+		
+		try {
+			return eSearch.search(tag, it.unipi.ing.mim.deep.Parameters.K);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+	
+	private List<ImgDescriptor> imageSearch(File image) {
+		
+		DNNExtractor extractor = new DNNExtractor();
+
+
+		imgFeatures = extractor.extract(image, it.unipi.ing.mim.deep.Parameters.DEEP_LAYER);
+
+		ImgDescriptor imDes = new ImgDescriptor(imgFeatures, openedImage.getName());
+		
+		try {	
+			return eSearch.search(imDes, it.unipi.ing.mim.deep.Parameters.K );
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+
+
+		
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
