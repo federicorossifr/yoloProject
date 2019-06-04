b16ef6698f311e9a1db01f50333086a3c8425194
Antonio Le Caldare <antonio.lecaldare@gmail.com>
2019-06-04 23:23:57 +0200

    yoloGUI: added transparency to bounding boxes. fixed text size and position. show bounding boxes also in serach result list

--- src/it/unipi/ing/mim/deep/ImageUtils.java
+++ src/it/unipi/ing/mim/deep/ImageUtils.java
@@ -2,13 +2,17 @@ package it.unipi.ing.mim.deep;
 
 import static org.bytedeco.opencv.global.opencv_imgproc.rectangle;
 import static org.bytedeco.opencv.global.opencv_imgproc.putText;
+import static org.bytedeco.opencv.global.opencv_highgui.*;
 import static org.bytedeco.opencv.global.opencv_imgproc.CV_FILLED;
+import static org.bytedeco.opencv.global.opencv_core.addWeighted;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.bytedeco.opencv.opencv_core.Point;
 import org.bytedeco.opencv.opencv_core.Rect;
@@ -16,7 +20,19 @@ import org.bytedeco.opencv.opencv_core.Scalar;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
 public class ImageUtils {
+	private final static double BBOX_ALPHA = 0.5;
+	private static Scalar[] colorSet = {
+			new Scalar(0,0,255,0.75),			
+			new Scalar(255,0,0,0.75),
+			new Scalar(60,120,20,0.75),
+			new Scalar(0,131,255,0.75),
+			new Scalar(255,0,255,0.75),
+			new Scalar(2,2,112,0.75)
+	};
 	
+	private static Scalar getRandomColor(int idx) {
+		return colorSet[idx%colorSet.length];
+	}
 	/**
 	 * Method to get a JavaFX image from an ImageDescriptor, with the relative bouding
 	 * box, (if any) applied.
@@ -30,12 +46,30 @@ public class ImageUtils {
 		int bboxIdx = i.getBoundingBoxIndex();
 		if(bboxIdx >= 0) {
 			int[] bboxCoords = di.getBoundingBoxByIndex(bboxIdx);
-			Rect roi = getRectFromCorners(bboxCoords);
-			rectangle(imageContent, roi, new Scalar(0.0,0.0,255.0,1),5,8,0);
-			//putText(imageContent,di.getClassByIndex(bboxIdx),new Point(bboxCoords[0]-3,bboxCoords[2]-3),1,1.0f,new Scalar(255.0,255.0,255.0,1));				
+			applyRectAndLabel(imageContent, bboxCoords, di.getClassByIndex(bboxIdx),getRandomColor(bboxIdx));	
 		} 
 
 		return matToImage(imageContent);
+	}
+	
+	private static void applyRectAndLabel(Mat imageContent,int[] bbox,String label,Scalar bgColor) {
+		Rect roi = getRectFromCorners(bbox);
+		int left=bbox[0],right=bbox[1],top=bbox[2],bottom=bbox[3];
+		//Outer bounding box
+		Mat overlay = new Mat();
+		imageContent.copyTo(overlay);
+		rectangle(overlay, roi, bgColor,2,8,0);
+		
+	
+		
+		//Label background box
+		Rect bg = new Rect(new Point(left,top),new Point(right,top+15));
+		rectangle(overlay,bg,bgColor,CV_FILLED,0,0);		
+		addWeighted(overlay,  BBOX_ALPHA, imageContent, 1- BBOX_ALPHA, 0, imageContent);
+
+		//Label
+		putText(imageContent,label,new Point(left,top+10),0,0.4f,new Scalar(255.0,255.0,255.0,1));	
+
 	}
 	
 	public static Image getDrawable(List<ImgDescriptor> li) throws IOException {
@@ -47,17 +81,11 @@ public class ImageUtils {
 			int bboxIdx = i.getBoundingBoxIndex();
 			if(bboxIdx >= 0) {
 				int[] bboxCoords = di.getBoundingBoxByIndex(bboxIdx);
-				Rect roi = getRectFromCorners(bboxCoords);
-				rectangle(imageContent, roi, new Scalar(0.0,0.0,255.0,1),2,3,0);
-
-				Rect bg = new Rect(new Point(bboxCoords[0],bboxCoords[2]-10),new Point(bboxCoords[0]+roi.width()+1,bboxCoords[2]));
-				rectangle(imageContent,bg,new Scalar(0.0,0.0,255.0,1),CV_FILLED,0,0);
-				putText(imageContent,di.getClassByIndex(bboxIdx),new Point(bboxCoords[0]-3,bboxCoords[2]-3),1,1.0f,new Scalar(255.0,255.0,255.0,1));				
+				applyRectAndLabel(imageContent, bboxCoords, di.getClassByIndex(bboxIdx),getRandomColor(bboxIdx));					
 			} 
 
 		}
-		//imshow(".",imageContent);
-		//waitKey();
+	
 		return matToImage(imageContent);		
 	}
 	
